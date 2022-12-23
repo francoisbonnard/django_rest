3a9f4c3a4980c578ba5276271b36af38b36458b8
francoisbonnard <francois.bonnard@gmail.com>
2022-12-23 18:31:05 +0100

    including basic test file

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator function
+"""
+
+def add(x,y):
+    """ Adds x and y and returns result. """
+    return x + y
+
+def subtract(x,y):
+    """ Subtracts x from y and returns result. """
+    return y - x 
